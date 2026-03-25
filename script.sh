36ce89da484fda576481ca317e278c6123f0c025
Aditya Narayan Singh <54374836+AdityaSingh25@users.noreply.github.com>
2026-03-26 01:58:48 +0530

    added -y flag

--- script.sh
+++ script.sh
@@ -6,12 +6,12 @@ sudo apt update
 
 # Install nodejs
 
-sudo apt install nodejs
+sudo apt install nodejs -y
 
 # Install npm in the maching
 
-sudo apt install npm
-sudo apt install pm2
+sudo apt install npm -y
+sudo apt install pm2 -y
 # clone the repo
 
 git clone https://github.com/AdityaSingh25/DemoAwsServer.git /home/ubuntu
@@ -22,6 +22,6 @@ cd home/ubuntu/DemoAwsServer/
 
 # install packages
 
-sudo npm install
+sudo npm install -y
 
 sudo npx pm2 start index.js
